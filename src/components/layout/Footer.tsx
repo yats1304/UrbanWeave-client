eab25a675a3e8d9f16c45100223428b448ea4187
yats1304 <chaubaly@gmail.com>
2026-01-10 14:30:23 +0530

    feat: navbar and footer is added

---
+++ src/components/layout/Footer.tsx
@@ -0,0 +1,50 @@
+"use client";
+
+import Image from "next/image";
+import Link from "next/link";
+
+const Footer = () => {
+  return (
+    <div className="mt-16 flex flex-col items-center gap-8 md:flex-row md:items-start md:justify-between md:gap-0 bg-gray-800 p-8 rounded-lg">
+      <div className="flex flex-col gap-4 items-center md:items-start">
+        {/* Logo with brand name */}
+        <Link href="/" className="flex items-center">
+          <Image
+            src="/logo.png"
+            alt="UrbanWeave"
+            height={36}
+            width={36}
+          ></Image>
+          <p className="hidden md:block text-md font-medium tracking-wider text-white">
+            UrbanWeave.
+          </p>
+        </Link>
+        <p className="text-sm text-gray-400">© 2026 UrbanWeave.</p>
+        <p className="text-sm text-gray-400">All rights reserved.</p>
+      </div>
+      <div className="flex flex-col gap-4 text-sm text-gray-400 items-center md:items-start">
+        <p className="text-amber-50">Links</p>
+        <Link href="/">Homepage</Link>
+        <Link href="/">Contact</Link>
+        <Link href="/">Terms of Service</Link>
+        <Link href="/">Private Policies</Link>
+      </div>
+      <div className="flex flex-col gap-4 text-sm text-gray-400 items-center md:items-start">
+        <p className="text-amber-50">Product</p>
+        <Link href="/">All Products</Link>
+        <Link href="/">New Arrivals</Link>
+        <Link href="/">Best Sellers</Link>
+        <Link href="/">Sale</Link>
+      </div>
+      <div className="flex flex-col gap-4 text-sm text-gray-400 items-center md:items-start">
+        <p className="text-amber-50">Company</p>
+        <Link href="/">About</Link>
+        <Link href="/">Contact</Link>
+        <Link href="/">Blog</Link>
+        <Link href="/">Affiliate Program</Link>
+      </div>
+    </div>
+  );
+};
+
+export default Footer;
